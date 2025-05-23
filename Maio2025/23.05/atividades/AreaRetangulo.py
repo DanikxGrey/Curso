3294a739cab51bb5dfa17673fffbbb34d24cd110
DanikxGrey <gabrieldaviadr88@gmail.com>
2025-05-23 11:13:22 -0300

    Add files via upload

---
+++ Maio2025/23.05/atividades/AreaRetangulo.py
@@ -0,0 +1,10 @@
+#Atividade 3
+#Calcular area de um retangulo
+
+base = int(input("Qual a base do triangulo? "))
+
+altura = int(input("Qual a altura do triangulo "))
+
+area = (base * altura)
+
+print ("A área do triangulo é", area, "cm" )
---
+++ Maio2025/23.05/atividades/NumerosNegativos.py
@@ -0,0 +1,14 @@
+#Atividade 8
+num = [0] * 4
+
+def NegativoCheck(cont):
+    cont = 0
+    while cont < 4:
+        if num[0] = 0 
+            print
+        
+        
+num [0] = float(input ("Digite o 1° valor") )
+num [1] = float(input ("Digite o 2° valor") )
+num [2] = float(input ("Digite o 3° valor") )
+num [3] = float(input ("Digite o 4° valor") )
