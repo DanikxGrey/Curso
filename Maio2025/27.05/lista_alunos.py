868eab6cc0137efb1d1d5ddd2161343bad4b7707
DanikxGrey <gabrieldaviadr88@gmail.com>
2025-05-27 11:10:40 -0300

    Add files via upload

---
+++ Maio2025/27.05/lista_alunos.py
@@ -0,0 +1,43 @@
+# Utilize ctrl + : para transformar codigo em comentario e vice versa
+
+alunos = ["rafaela", "Pedro", "Bianca", "Jessica", "Bruno"]
+
+# print (alunos[2])
+# print (alunos[3])
+# print (alunos)
+
+# for alunos in alunos:
+#     print(alunos)
+
+
+# alunos.append('Helena')
+# # Adiciona um elemento ao final da lista.
+# print(alunos)
+
+
+# alunos.insert (3, "Daniela")
+# # Insert adiciona uma string a uma posição da variavel
+# # Insere um elemento em uma posição específica da lista, deslocando os elementos subsequentes para a direita.
+# print (alunos)
+
+
+# removido = alunos.pop(2)  # Remove o elemento na posição 2 ("Bianca")
+# # Remove e retorna o elemento na posição especificada. Se nenhum índice for fornecido, remove o último elemento.
+# print(alunos)  # Saída: ['Rafaela', 'Pedro', 'Jessica', 'Bruno']
+# print(removido)  # Saída: Bianca
+# alunos.pop()  # Remove o último elemento ("Bruno")
+# print(alunos)  # Saída: ['Rafaela', 'Pedro', 'Jessica']
+# print(removido)
+
+
+# alunos.remove("Bianca")  # Remove a primeira ocorrência de "Bianca"
+# # Remove a primeira ocorrência do elemento especificado na lista. 
+# # Levanta um erro ValueError se o elemento não estiver presente.
+# print(alunos)  # Saída: ['Rafaela', 'Pedro', 'Jessica', 'Bruno']
+
+
+# alunos.clear()  # Remove todos os elementos
+# # Remove todos os elementos da lista, deixando-a vazia.
+# print(alunos)  # Saída: []
+
+
