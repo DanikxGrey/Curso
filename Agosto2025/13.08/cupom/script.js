c26e5d2f9a8c7743488e1f53a9d8717c97bed151
DanikxGrey <gabrieldaviadr88@gmail.com>
2025-08-13 11:22:50 -0300

    Add files via upload

---
+++ Agosto2025/13.08/cupom/script.js
@@ -0,0 +1,4 @@
+function mudarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.innerHTML= "Parabéns, você acaba de ganhar 5% de desconto em todos os produtos! <br> aproveite!">;
+}
