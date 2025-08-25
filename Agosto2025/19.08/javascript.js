60d625127cd6cb0e004420862c5ddbf45e3911b8
DanikxGrey <gabrieldaviadr88@gmail.com>
2025-08-25 11:28:38 -0300

    Update javascript.js

--- Agosto2025/19.08/javascript.js
+++ Agosto2025/19.08/javascript.js
@@ -20,11 +20,34 @@ function VerificarLogin() {
 
     if (AcessoPermitido === true) {
         resultado.innerHTML = "Acesso Permitido";
+        setTimeout(() => {
+            resultado.innerHTML = "Redirecionando..."
+        }, 500);
+
+        setTimeout(() => {
+            resultado.innerHTML = "Finalizado"
+        }, 750);
+
+        setInterval(() => {
+            window.location.href = "https://deepwoken.co/builder"
+        }, 1000);
     }
+
     else {
         resultado.innerHTML = "Acesso Negado";
+
+        setTimeout(() => {
+           resultado.innerHTML = "Tente novamente" 
+        }, 1500);
+        
+// Limpa os campos de texto
+        login.value = ""
+        senha.value = ""
+
     }
-}
+
+} // fim da funçao
+
 
 //    const input = document.getElementById("login").ariaValueMax;
 //    const mensagem = document.getElementById("mensagem");
