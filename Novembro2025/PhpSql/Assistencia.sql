d8235301fd3db6e765c55173db98df7fcfb2b20c
DanikxGrey <gabrieldaviadr88@gmail.com>
2025-11-03 11:43:21 -0300

    Delete Novembro2025/PhpSql directory

--- Novembro2025/PhpSql/Assistencia.sql
+++
@@ -1,9 +0,0 @@
-create table agendamentos(
-id int auto_increment primary key,
-nome varchar (100) not null,
-telefone varchar(20) not null,
-aparelho varchar(50) not null,
-data date not null,
-descricao text not null,
-data_envio timestamp default current_timestamp
-);
